3d337f5264a92e4b12efd971c43df91ef0991c17
Alperen Çapar <alperencapar2@gmail.com>
2022-11-27 20:31:36 +0300

    spacing

--- blog/urls.py
+++ blog/urls.py
@@ -10,9 +10,9 @@ from .views import (
 
 urlpatterns = [
     path('',home, name="blog-home"),
-    path('article/<slug:slug>/',article_detail, name="article_detail"),
-    path('create-article/',create_article, name="article_create"),
-    path('update-article/<slug:slug>',update_article, name="article_update"),
-    path('category/',categories_home, name="categories_home"),
-    path('category/<slug:category_name>/',category_articles, name="category_detail"),
+    path('article/<slug:slug>/', article_detail, name="article_detail"),
+    path('create-article/', create_article, name="article_create"),
+    path('update-article/<slug:slug>', update_article, name="article_update"),
+    path('category/', categories_home, name="categories_home"),
+    path('category/<slug:category_name>/', category_articles, name="category_detail"),
 ]
